08397e2d1a4cf3e9c00a427bbaaaddc7b8ed9de5
filipzag <filipz0203@gmail.com>
2024-05-07 00:12:48 +0200

    added database functionality

--- database.py
+++ database.py
@@ -1,16 +1,23 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, create_engine
+import os
 
-
+DB_USER = os.environ.get('PS_USER')
+DB_PASS = os.environ.get('PS_PASS')
 
 class Entry(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    secret_name: 
-    age: Optional[int] = None
-
     name:str
     others: str
-    accomodation: str
-    menu: str
+    menu: bool
     info: str
+
+connection_string = f"postgresql://{DB_USER}:{DB_PASS}@localhost:5432/wedding_people"
+
+def get_engine():
+    engine = create_engine(connection_string)
+    return engine
+
+engine = create_engine(connection_string)
+SQLModel.metadata.create_all(engine)
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from database import get_engine, Entry
+from sqlmodel import Session
 
+database_engine = get_engine()
 
 origins = [
     "http://localhost:3000",
@@ -28,8 +31,6 @@ class Item(BaseModel):
     info: str
 
 
-
-
 @app.get("/")
 async def read_root():
     return {"Nothing": "here"}
@@ -37,4 +38,15 @@ async def read_root():
 
 @app.post("/items/create")
 async def save_item(item: Item):
-    return {"item_name": item.name, "others": item.others, "accomodation": item.accomodation}
+
+    person = Entry(name=item.name, others=item.others, menu=item.menu, info=item.info)
+
+    with Session(database_engine) as session:
+        try:
+            session.add(person)
+            session.commit()
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=400, detail="Can't save item to database!")
+            
+
